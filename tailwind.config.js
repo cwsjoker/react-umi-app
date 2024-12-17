91184f2cb0a24494378b94cb0565be0bceb07ac0
clear <clear@123.com>
2024-12-17 18:32:34 +0800

    feat：px简写转化

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+
+const genSizeMap = (size) =>
+  Array.from({ length: size }).reduce(
+    (map, _, i) => ({
+      ...map,
+      [i]: `${i}px`,
+    }),
+    {}
+  );
+
 module.exports = {
   content: [
-    './src/pages/**/*.tsx',
-    './src/components/**/*.tsx',
-    './src/layouts/**/*.tsx',
+    "./src/pages/**/*.tsx",
+    "./src/components/**/*.tsx",
+    "./src/layouts/**/*.tsx",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'cbd': 'linear-gradient(90deg, rgba(255, 0, 0, 1) 0%, rgba(0, 0, 255, 1) 50%, rgba(255, 0, 0, 1) 100%)'
-      }
+        cbd: "linear-gradient(90deg, rgba(255, 0, 0, 1) 0%, rgba(0, 0, 255, 1) 50%, rgba(255, 0, 0, 1) 100%)",
+      },
+      backgroundSize: {
+        'size-full': '100% 100%'
+      },
+      fontSize: { ...genSizeMap(100) },
+      padding: { ...genSizeMap(100) },
+      spacing: { ...genSizeMap(100) },
+      borderRadius: { ...genSizeMap(100) },
+      width: {
+        ...genSizeMap(100),
+        full: '100%',
+      },
+      height: {
+        ...genSizeMap(100),
+        full: '100%',
+      },
+      lineHeight: { ...genSizeMap(100) },
+      minHeight: {
+        ...genSizeMap(100),
+        contentHeight: 'calc(100vh - 48px)',
+      },
     },
   },
   plugins: [],
-}
-
+};
