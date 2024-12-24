244c35225228f7b2596d510b0ae55c37fb7d1e18
clear <clear@123.com>
2024-12-24 14:04:19 +0800

    feat: typescript

--- src/pages/tsRule/index.tsx
+++ src/pages/tsRule/index.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
 
 
+type Todo = {
+    name: string;
+    age: number;
+    height: number
+}
+// pick
+type PersonPick = Pick<Todo, 'name' | 'age'>
+// omit
+type PersonOmit = Omit<Todo, 'height'>
+// Partial
+type PersonPartial = Partial<Todo>
+// Required
+type PersonRequired = Required<Omit<Todo, 'height'>>
+// Record
+type PersonRecord = Record<'name' | 'age', string>
+// NonNullable
+// type PersonNonNullable = NonNullable<string | number | undefined>
 
 const Index = () => {
+
+    const a:Todo = { name: 'clear', age: 18, height: 180 }
+
+    const b:PersonPartial = { name: 'clear123'}
+
+    const c:PersonPick = { name: 'clear', age: 123 }
+
+    const d:PersonOmit = { name: 'clear', age: 123 }
+
+    const e:PersonRequired = { name: 'clear', age: 18 }
+
+    const f:Partial<PersonRecord> = { name: 'clear' }
+
     return (
         <div>ts</div>
     )
